766003d454fbf8fbeca037a600ac322a83672874
wagnersouz4 <wagnersouz4@gmail.com>
2017-03-21 06:16:55 -0300

    adding TmDbAPI

---
+++ Replay/Replay/Networking/TMDbAPI.swift
@@ -0,0 +1,71 @@
+//
+//  TMDbAPI.swift
+//  Replay
+//
+//  Created by Wagner Souza on 21/03/17.
+//  Copyright © 2017 Wagner Souza. All rights reserved.
+//
+
+import Moya
+
+enum TMDbService {
+    case movie(id: Int)
+}
+
+extension TMDbService: TargetType {
+
+    var baseURL: URL { return URL(string: "https://api.themoviedb.org/3")! }
+
+    var path: String {
+        switch self {
+        case .movie(let id):
+            return "/movie/\(id)"
+        }
+    }
+
+    var method: Moya.Method {
+        return .get
+    }
+
+    var parameters: [String: Any]? {
+        return ["api_key": "YOUR_API_KEY_HERE",
+                "append_to_response": "videos,images"]
+    }
+
+    var parameterEncoding: ParameterEncoding {
+        /// Parameters that should sent in the URL.
+        return URLEncoding.default
+
+    }
+
+    /// Provides stub data as response for use in testing.
+    var sampleData: Data {
+        switch self {
+        case .movie:
+            return stubbedResponse("Movie")
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .movie:
+            return .request
+        }
+    }
+
+    /// Using Alamofire auto response validation
+    /// See: https://github.com/Alamofire/Alamofire#response-validation
+    var validate: Bool {
+        return true
+    }
+
+}
+
+func stubbedResponse(_ filename: String) -> Data {
+    /// Searching for the file, if not found or the file is not recognized as a valid Base-64 returning an empty Data
+    guard let path = Bundle.main.path(forResource: filename, ofType: "json"),
+        let data = Data(base64Encoded: path) else {
+            return Data()
+    }
+    return data
+}
