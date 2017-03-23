e8ef1e7f1778ff229f6e3100c945afd1ed41ca51
wagnersouz4 <wagnersouz4@gmail.com>
2017-03-22 22:55:23 -0300

    adding a unit test to the model genre

---
+++ Replay/ReplayTests/Model/GenreTests.swift
@@ -0,0 +1,23 @@
+//
+//  GenreTests.swift
+//  Replay
+//
+//  Created by Wagner Souza on 22/03/17.
+//  Copyright © 2017 Wagner Souza. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable
+import Replay
+
+class GenreTests: QuickSpec {
+    override func spec() {
+        it("should create a Genre from a JSON") {
+            let name = "Comedy"
+            let data = ["name": name]
+            guard let genre = Genre(json: data) else { return fail() }
+            expect(genre.name) == name
+        }
+    }
+}
